4f7dc529c4c60222b5254d6eec7bbd4b9c859d7d
Igor Knop <igorknop@gmail.com>
2021-01-12 07:49:54 -0300

    Add line generation

--- octangled.mjs
+++ octangled.mjs
@@ -5,12 +5,70 @@ window.onload = () => {
   document.body.appendChild(canvas);
   const ctx = canvas.getContext("2d");
   drawBackground(ctx, canvas);
-  const plugs = [{ x: 200, y: 200, t: "O" }];
+  const plugs = [
+    //  { x: 200, y: 200, t: "O" }
+  ];
   genPlugs(plugs, 8);
+  const lines = [];
+  genLines(plugs, lines, 400 * 3);
   plugs.forEach((p) => {
     drawPlug(ctx, p);
   });
+  lines.forEach((l) => drawLine(ctx, l));
+  const total = lines.reduce((t, c) => t + c.size, 0);
+  ctx.font = "25px Impact";
+  ctx.fillText(`${Math.floor((total * 8) / 400)}cm ${lines.length} steps`, 10, 380);
 };
+
+function drawLine(ctx, l) {
+  ctx.lineWidth = 5;
+  if (l.top) {
+    ctx.strokeStyle = "blue";
+  } else {
+    ctx.strokeStyle = "red";
+    ctx.globalAlpha = 0.5;
+  }
+  ctx.beginPath();
+  ctx.moveTo(l.s.x, l.s.y);
+  ctx.lineTo(l.t.x, l.t.y);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.globalAlpha = 1.0;
+}
+
+function genLines(plugs, lines, max) {
+  let total = 0;
+  let source = Math.floor(Math.random() * plugs.length);
+  let psource = source;
+  let target;
+  let top = true;
+  let step = 1;
+  plugs[source].steps.push(step++);
+  while (total < max && step < 20) {
+    target = Math.floor(Math.random() * plugs.length);
+    while (source === target || psource === target) {
+      target = Math.floor(Math.random() * plugs.length);
+    }
+    const dist = Math.sqrt(
+      Math.pow(plugs[source].x - plugs[target].x, 2) +
+        Math.pow(plugs[source].y - plugs[target].y, 2)
+    );
+    lines.push({
+      s: plugs[source],
+      t: plugs[target],
+      top,
+      step: step,
+      size: dist,
+    });
+    plugs[target].steps.push(step);
+    total += dist;
+    step++;
+    top = !top;
+    psource = source;
+    source = target;
+  }
+  console.table(lines);
+}
 
 function genPlugs(plugs, n) {
   let t = 1;
@@ -21,6 +79,7 @@ function genPlugs(plugs, n) {
       t: t++,
       x: 200 + radius * Math.cos(i * k + k / 2),
       y: 200 + radius * Math.sin(i * k + k / 2),
+      steps: [],
     };
     plugs.push(plug);
   }
@@ -42,4 +101,10 @@ function drawPlug(ctx, plug) {
   ctx.fillStyle = "white";
   ctx.font = "20px Impact";
   ctx.fillText(plug.t, plug.x + 7, plug.y + 34);
+  ctx.globalAlpha = 1.0;
+  ctx.font = "10px Impact";
+  ctx.fillStyle = "white";
+  plug.steps.forEach((s, k) => {
+    ctx.fillText(`${s},`, plug.x + 10+ 12 * (1 + k), plug.y+34);
+  });
 }
